6348d1f4967ccbc4bfd3c4e32cb35b4d2de6a4e2
ColinLouis95 <colinkugler4@gmail.com>
2025-02-18 20:45:46 -0700

    Update to Search bar look.

--- Athenaeum/Views/GroupView.swift
+++ Athenaeum/Views/GroupView.swift
@@ -8,6 +8,58 @@
 import SwiftUI
 import SwiftData
 
+struct SortingEditView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @Environment(\.modelContext) private var modelContext
+    @Environment(\.dismiss) private var dismiss
+    @Binding var isPresented: Bool
+    
+    let backgroundColor = Color(#colorLiteral(red: 0.09300225228, green: 0.10428413, blue: 0.4961095452, alpha: 0.8674461921))
+    let textFieldColor = Color(#colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1))
+    
+    @State var name = ""
+    @State var pass = ""
+    @State var site = ""
+    @State var info = ""
+    
+    func saveData () {
+        let newPass = UserInfo(username: name, password: pass, site: site, notes: info)
+        modelContext.insert(newPass)
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Username", text: $name)
+                TextField("Password", text: $pass)
+                TextField("Site", text: $site)
+                TextField("Notes", text: $info)
+            }
+            .scrollContentBackground(.hidden).background(backgroundColor)
+            .navigationTitle("New Password")
+            .toolbarColorScheme(.dark, for: .navigationBar)
+            .toolbarBackground(backgroundColor, for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Save") {
+                        saveData()
+                        dismiss()
+                    }
+                }
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel") {
+                        dismiss()
+                        
+                    }
+                }
+                
+            }
+            
+        }
+    }
+}   // end of NewPasswordView
+
 struct NewPasswordView: View {
     @Environment(\.presentationMode) var presentationMode
     @Environment(\.modelContext) private var modelContext
@@ -91,7 +143,7 @@ struct viewPassword: View {
 struct GroupView: View {
     let backgroundColor = Color(#colorLiteral(red: 0.09300225228, green: 0.10428413, blue: 0.4961095452, alpha: 0.8674461921))
     let buttonColor = Color(#colorLiteral(red: 0.02285457216, green: 0.01912862621, blue: 0.9728235602, alpha: 0.9074658526))
-    let textFieldColor = Color(#colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1))
+    let textFieldColor = Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
     
     @Query var userInputs: [UserInfo]
     
@@ -99,6 +151,7 @@ struct GroupView: View {
     @State var searchText: String = ""
     @State var toggleGroup: Bool = false
     @State var isShowingNewPassView = false
+    @State var isShowingSortingEditView = false
 
 // Creates a white color font for Navigation Header
     init() {
@@ -122,6 +175,7 @@ struct GroupView: View {
                             .foregroundStyle(Color.gray)
 
                         TextField("Search", text: $searchText)
+                            .foregroundStyle(Color.white)
                             
                         
                         if searchText.isEmpty {
@@ -132,11 +186,11 @@ struct GroupView: View {
                     } // End of HStack
                     
                     .padding()
-                    .background(textFieldColor.cornerRadius(25))
-                    .foregroundColor(.white)
+                    .background(textFieldColor.cornerRadius(15).opacity(0.8))
                     .font(.headline)
-                    .accentColor(.purple)
-                    .shadow(color: .purple, radius: 2)
+                    .accentColor(.green)
+                    .shadow(color: .black.opacity(0.3), radius: 10, x: 10, y: 10)
+                    .blur(radius: 0.2, opaque: false)
                     
                     Spacer()
                     
@@ -181,18 +235,23 @@ struct GroupView: View {
                     HStack {    // for making the buttons on the bottom for adding new password or for changing order of passwords
                     
                         Button(action: {
-                            
+                            isShowingSortingEditView = true
                         }, label: {
                             Image(systemName: "slider.vertical.3")
                                 .foregroundStyle(Color.white)
                                 .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).bold()
+                        })
+                        .sheet(isPresented: $isShowingSortingEditView, content: {
+                            NewPasswordView(isPresented: $isShowingSortingEditView)
+                                
                         })
                         
                         Spacer()
                             .frame(width: 100)
                             .background(Color.orange)
                         
-                        Text("Number of passwords total")
+                        Text("\(userInputs.count) Passwords")
+                            .foregroundStyle(Color.white)
 
                         
                         Spacer()
@@ -212,12 +271,11 @@ struct GroupView: View {
                         })
                         
                     } // End of HStack
-                    .background(Color.gray)
                     .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
                     .opacity(0.8)
                   
                 } // End of VStack
-                .navigationTitle("Athenaeum")
+                .navigationTitle("Passwords")
                 
                 
                 
